9d795537829f48015340a4013f473a4256e65d06
Abdurazaq333 <irazaqr986@gmail.com>
2026-05-12 12:26:21 +0500

    signup error resolve

--- aurum-fintech/src/App.jsx
+++ aurum-fintech/src/App.jsx
@@ -2,16 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
+
 // import Dashboard from "./pages/Dashboard";
 import Markets from "./pages/Markets";
+import Signup from "./pages/SignUp";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      {/* <Route path="/signup" element={<Signup />} /> */}
+      <Route path="/signup" element={<Signup />} />
       {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       <Route path="/markets" element={<Markets />} />
     </Routes>
